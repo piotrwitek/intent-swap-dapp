87040eab53a9a13021c976a694658780c7d0cfb3
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2025-07-14 16:07:38 +0200

    refactor: simplify App component structure by removing unnecessary providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AppProvider } from "./context/AppContext";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Orders from "./pages/Orders";
 import OrderDetails from "./pages/OrderDetails";
 import "./App.css";
-import { Providers } from "./providers";
+import { useUser } from "@account-kit/react";
 
 function App() {
+  const user = useUser();
   return (
-    <Providers>
-      <AppProvider>
-        <Router>
-          <Layout>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/orders" element={<Orders />} />
-              <Route path="/orders/:orderId" element={<OrderDetails />} />
-            </Routes>
-          </Layout>
-        </Router>
-      </AppProvider>
-    </Providers>
+    <Router key={user?.address || user?.email || "guest"}>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/orders/:orderId" element={<OrderDetails />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./main.css";
 import App from "./App.tsx";
+import { Providers } from "./providers";
+import { AppProvider } from "./context/AppContext";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Providers>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </Providers>
   </StrictMode>
 );
